00bf24598b8a898497d8aafff74c46dde1890c15
Wesley <podwesley@gmail.com>
2023-01-05 17:44:20 -0300

    criação do projeto

---
+++ src/main/java/com/talent/MainTest.java
@@ -0,0 +1,49 @@
+package com.talent;
+
+import io.appium.java_client.MobileElement;
+import io.appium.java_client.android.AndroidDriver;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.openqa.selenium.remote.DesiredCapabilities;
+
+import java.net.MalformedURLException;
+import java.net.URL;
+
+public class MainTest {
+
+    private AndroidDriver driver;
+
+    @Before
+    public void setUp() throws MalformedURLException {
+        DesiredCapabilities desiredCapabilities = new DesiredCapabilities();
+
+        desiredCapabilities.setCapability("platformName", "Android");
+
+            desiredCapabilities.setCapability("appium:deviceName", "emulator");
+
+
+        desiredCapabilities.setCapability("appium:automationName", "uiautomator2");
+        desiredCapabilities.setCapability("appium:appPackage", "com.ctappium");
+        desiredCapabilities.setCapability("appium:appActivity", "com.ctappium.MainActivity");
+
+
+        URL remoteUrl = new URL("http://127.0.0.1:4723/wd/hub");
+
+        driver = new AndroidDriver<MobileElement>(remoteUrl, desiredCapabilities);
+
+    }
+
+    @Test
+    public void sampleTest() {
+
+    }
+
+
+
+
+    @After
+    public void tearDown() {
+        driver.quit();
+    }
+}
