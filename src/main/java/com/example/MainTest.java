0dee363130594e8e6d6146949abecf2e26726f3d
Wesley <podwesley@gmail.com>
2023-01-06 17:04:30 -0300

    refactor

--- src/main/java/com/example/MainTest.java
+++ src/main/java/com/example/MainTest.java
@@ -3,6 +3,7 @@ package com.example;
 import io.appium.java_client.MobileBy;
 import io.appium.java_client.MobileElement;
 import io.appium.java_client.android.AndroidDriver;
+import io.appium.java_client.remote.MobileCapabilityType;
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
@@ -24,11 +25,11 @@ public class MainTest {
         DesiredCapabilities desiredCapabilities = new DesiredCapabilities();
 
         desiredCapabilities.setCapability("platformName", "Android");
-        desiredCapabilities.setCapability("appium:deviceName", "emulator-5554"); //para ver os nomes dos dispositivos: adb devices
+        desiredCapabilities.setCapability("appium:deviceName", "emulator"); //para ver os nomes dos dispositivos: adb devices
         desiredCapabilities.setCapability("appium:automationName", "uiautomator2");
         desiredCapabilities.setCapability("appium:appPackage", "com.ctappium");
         desiredCapabilities.setCapability("appium:appActivity", "com.ctappium.MainActivity");
-        // desiredCapabilities.setCapability(MobileCapabilityType.APP, Utils.findFilesResources("CTAppium_2_0.apk")); // Verifica se o app esta instalado caso negativo instala caso positivo reseta o app.
+        desiredCapabilities.setCapability(MobileCapabilityType.APP, Utils.findFilesResources("CTAppium_2_0.apk")); // Verifica se o app esta instalado caso negativo instala caso positivo reseta o app.
 
         URL remoteUrl = new URL("http://127.0.0.1:4723/wd/hub");
 
@@ -40,7 +41,7 @@ public class MainTest {
     @Test
     public void editText() {
 
-        //class[@class='android.widget.TextView']
+        //android.widget.TextView[@text='Formulário']
         List<MobileElement> elements = driver.findElements(By.className("android.widget.TextView"));
 
         elements.get(1).click();
