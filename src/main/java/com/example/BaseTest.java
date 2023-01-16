d29c3e64ec8a561ac8cc55c8ac6d3c93383dab75
Wesley <podwesley@gmail.com>
2023-01-16 17:05:58 -0300

    refactor

--- src/main/java/com/example/BaseTest.java
+++ src/main/java/com/example/BaseTest.java
@@ -1,4 +1,70 @@
 package com.example;
 
+import io.appium.java_client.MobileElement;
+import io.appium.java_client.android.AndroidDriver;
+import org.junit.Assert;
+import org.junit.Before;
+import org.openqa.selenium.By;
+import org.openqa.selenium.remote.DesiredCapabilities;
+
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.concurrent.TimeUnit;
+
 public class BaseTest {
+
+    protected AndroidDriver driver;
+
+    @Before
+    public void setUp() throws MalformedURLException {
+        DesiredCapabilities desiredCapabilities = new DesiredCapabilities();
+
+        desiredCapabilities.setCapability("platformName", "Android");
+        desiredCapabilities.setCapability("appium:deviceName", "emulator-5554"); //para ver os nomes dos dispositivos: adb devices
+        desiredCapabilities.setCapability("appium:automationName", "uiautomator2");
+        desiredCapabilities.setCapability("appium:appPackage", "com.ctappium");
+        desiredCapabilities.setCapability("appium:appActivity", "com.ctappium.MainActivity");
+        //desiredCapabilities.setCapability(MobileCapabilityType.APP, Utils.findFilesResources("CTAppium_2_0.apk")); // Verifica se o app esta instalado caso negativo instala caso positivo reseta o app.
+        desiredCapabilities.setCapability("newCommandTimeout", 1000); // wait for 1000 milliseconds to run new command
+
+        URL remoteUrl = new URL("http://127.0.0.1:4723/wd/hub");
+
+        driver = new AndroidDriver<MobileElement>(remoteUrl, desiredCapabilities);
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS); // remover este wait implicito.
+
+    }
+
+    public void acessarMenuPrincipal(String menu) {
+        MobileElement element = (MobileElement) driver.findElement(By.xpath("//*[@text='" + menu + "']"));
+        element.click();
+    }
+
+    public void buttonCheckedAndSwitch (MobileElement element, String value) {
+
+        if(!element.getAttribute("checked").equals(value))
+            element.click();
+        Assert.assertEquals(value, element.getAttribute("checked"));
+    }
+
+    public void click(MobileElement element) {
+        element.click();
+    }
+
+    /***
+     * This method types the value into a field of type input
+     * @param element
+     * @param value
+     * @return
+     */
+    public void typeValue(MobileElement element, String value) {
+        element.sendKeys(value);
+    }
+
+    public void xpathExamples() {
+
+        /**
+         * //android.widget.TextView[starts-with(@text, 'Console:')]
+         */
+
+    }
 }
--- src/main/java/com/example/MainTest.java
+++ src/main/java/com/example/MainTest.java
@@ -2,44 +2,13 @@ package com.example;
 
 import io.appium.java_client.MobileBy;
 import io.appium.java_client.MobileElement;
-import io.appium.java_client.android.AndroidDriver;
-import io.appium.java_client.remote.MobileCapabilityType;
-import org.junit.After;
 import org.junit.Assert;
-import org.junit.Before;
 import org.junit.Test;
 import org.openqa.selenium.By;
-import org.openqa.selenium.WebElement;
-import org.openqa.selenium.remote.DesiredCapabilities;
 
-import java.net.MalformedURLException;
-import java.net.URL;
 import java.util.List;
-import java.util.concurrent.TimeUnit;
 
-public class MainTest {
-
-    private AndroidDriver driver;
-
-    @Before
-    public void setUp() throws MalformedURLException {
-        DesiredCapabilities desiredCapabilities = new DesiredCapabilities();
-
-        desiredCapabilities.setCapability("platformName", "Android");
-        desiredCapabilities.setCapability("appium:deviceName", "emulator-5554"); //para ver os nomes dos dispositivos: adb devices
-        desiredCapabilities.setCapability("appium:automationName", "uiautomator2");
-        desiredCapabilities.setCapability("appium:appPackage", "com.ctappium");
-        desiredCapabilities.setCapability("appium:appActivity", "com.ctappium.MainActivity");
-        //desiredCapabilities.setCapability(MobileCapabilityType.APP, Utils.findFilesResources("CTAppium_2_0.apk")); // Verifica se o app esta instalado caso negativo instala caso positivo reseta o app.
-        desiredCapabilities.setCapability("newCommandTimeout", 1000); // wait for 1000 milliseconds to run new command
-
-
-        URL remoteUrl = new URL("http://127.0.0.1:4723/wd/hub");
-
-        driver = new AndroidDriver<MobileElement>(remoteUrl, desiredCapabilities);
-        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS); // remover este wait implicito.
-
-    }
+public class MainTest extends BaseTest{
 
     @Test
     public void editText() {
@@ -85,7 +54,6 @@ public class MainTest {
         Assert.assertEquals("true", switc.getAttribute("checked"));
         System.out.println("teste");
 
-
         //Verificar estados alterados
         check.click();
         switc.click();
@@ -104,26 +72,6 @@ public class MainTest {
 
         buttonCheckedAndSwitch(check, "false");
         buttonCheckedAndSwitch(switc, "false");
-
-    }
-    
-    public void acessarMenuPrincipal(String menu) {
-        MobileElement element = (MobileElement) driver.findElement(By.xpath("//*[@text='" + menu + "']"));
-        element.click();
-    }
-
-    public void buttonCheckedAndSwitch (MobileElement element, String value) {
-
-        if(!element.getAttribute("checked").equals(value))
-            element.click();
-        Assert.assertEquals(value, element.getAttribute("checked"));
-    }
-
-    public void xpathExamples() {
-
-        /**
-         * //android.widget.TextView[starts-with(@text, 'Console:')]
-         */
 
     }
 
